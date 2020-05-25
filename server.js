1272deb16194ee448ec83ea1b5dcf0957cdd4bcc
Nyonjee Zeogar <nyonjeezeogar@gmail.com>
2020-05-25 10:01:34 -0500

    Update package and module

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
+const Rollbar = require("rollbar");
+
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -11,7 +13,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const rollbar = new Rollbar({
+  accessToken: 'ecf0bf0995034f20a27dfa9f99c354a2',
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
 
 const db = require("./models");
 
@@ -19,5 +30,5 @@ require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log('App running on port ${PORT}!');
 });
